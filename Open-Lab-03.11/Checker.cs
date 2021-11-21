a8ea283916853915630591ed478c1964a987c5b6
cigan pocita <prismite741@gmail.com>
2021-11-21 22:35:28 +0100

    hotova funkcia

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
-        } 
+            if (str.Length <= 1)
+                return true;
+            else
+            {
+                if (str[0] != str[str.Length - 1])
+                    return false;
+                else
+                    return IsPalindrome(str.Substring(1, str.Length - 2));
+            }
+        }
     }
 }
